c118c2ebb1501cdb291622f7859fc170dbd8b6bd
Daniel García Muñoz <63578276+Daniel20091999@users.noreply.github.com>
2024-06-06 17:48:00 +0200

    git commit -m 'añadiendo los direntes tipos de servicios CRUD'

--- src/routes/todo.py
+++ src/routes/todo.py
@@ -1,23 +1,24 @@
 from flask import Blueprint
+from services.todo import create_todo_service, get_todos_service, get_todo_service_by_id, upadate_service, delete_todo_service
 
 todo = Blueprint('todo', __name__)
 
 @todo.route('/', methods =['GET'])
 def get_todos():
-    return 'get all todos'
+    return get_todos_service()
 
 @todo.route('/<id>', methods = ['GET'])
 def get_todo(id):
-    return 'Get todo by id'
+    return get_todo_service_by_id(id)
 
 @todo.route('/', methods = ['POST'])
 def create_todo():
-    return 'Create todo'
+    return create_todo_service()
 
 @todo.route ('/<id>' , methods = ['PUT'])
 def update_todo(id):
-    return 'Update todo'
+    return upadate_service(id)
 
 @todo.route ('/<id>' , methods = ['DELETE'])
 def delete_todo(id):
-    return 'Delete todo'
+    return delete_todo_service(id)
